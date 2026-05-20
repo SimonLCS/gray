6f119c347232c6368be97f2718e86542bab69cd2
Yohan Poirier-Ginter <pg.yohan@gmail.com>
2026-05-20 14:33:38 -0400

    migrate wget -> curl for windows support

--- scripts/download_all_pretrained_scenes.sh
+++ scripts/download_all_pretrained_scenes.sh
@@ -6,7 +6,7 @@ mkdir -p output/pretrained
 echo Downloading scenes to \`output/pretrained\`...
 
 for scene in bicycle bonsai counter drjohnson flowers garden kitchen playroom room stump train treehill truck; do 
-    wget https://repo-sam.inria.fr/nerphys/gray/pretrained/$scene.zip -O output/pretrained/$scene.zip
+    curl -fL --progress-bar -o output/pretrained/$scene.zip https://repo-sam.inria.fr/nerphys/gray/pretrained/$scene.zip
     python -m zipfile -e output/pretrained/$scene.zip output/pretrained/$scene
     rm output/pretrained/$scene.zip
 done
--- scripts/download_all_scenes.sh
+++ scripts/download_all_scenes.sh
@@ -5,17 +5,17 @@ mkdir -p data
 
 echo Downloading scenes to \`data/\`...
 
-wget -q --show-progress -P data/ http://storage.googleapis.com/gresearch/refraw360/360_v2.zip
+curl -fL --progress-bar -o data/360_v2.zip https://storage.googleapis.com/gresearch/refraw360/360_v2.zip
 python -m zipfile -e data/360_v2.zip data/360_v2/
 rm data/360_v2.zip
 rm data/360_v2/flowers.txt
 rm data/360_v2/treehill.txt
 
-wget  -q --show-progress -P data/ https://storage.googleapis.com/gresearch/refraw360/360_extra_scenes.zip
+curl -fL --progress-bar -o data/360_extra_scenes.zip https://storage.googleapis.com/gresearch/refraw360/360_extra_scenes.zip
 python -m zipfile -e data/360_extra_scenes.zip data/360_v2/
 rm data/360_extra_scenes.zip
 
-wget  -q --show-progress -P data/ https://repo-sam.inria.fr/fungraph/3d-gaussian-splatting/scenes/input/tandt_db.zip 
+curl -fL --progress-bar -o data/tandt_db.zip https://repo-sam.inria.fr/fungraph/3d-gaussian-splatting/scenes/input/tandt_db.zip
 python -m zipfile -e data/tandt_db.zip data/
 rm data/tandt_db.zip
 
